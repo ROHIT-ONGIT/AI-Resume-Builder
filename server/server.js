f69df40319135a355fcc91e79d6c9edae1ec106a
Rohit Kumar Vishwakarma <kumarrohitvishwa9936@gmail.com>
2026-02-03 18:25:12 +0530

    fix cors

--- server/server.js
+++ server/server.js
@@ -1,28 +1,16 @@
-import express from "express";
-import cors from "cors";
-import "dotenv/config";
-import connectDB from "./configs/db.js";
-import userRouter from "./routes/userRoutes.js";
-import resumeRouter from "./routes/resumeRoutes.js";
-import aiRouter from "./routes/aiRoutes.js";
+await connectDB();
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-// Database connection
-await connectDB()
-
-app.use(express.json())
 app.use(cors({
-    origin: "https://ai-resume-builder-0-omlz.vercel.app/"
+  origin: "https://ai-resume-builder-0-omlz.vercel.app",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
-app.get('/', (req, res)=> res.send("Server is live..."))
-app.use('/api/users', userRouter)
-app.use('/api/resumes', resumeRouter)
-app.use('/api/ai', aiRouter)
+app.use(express.json());
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`);
-    
-});
+app.get("/", (req, res) => res.send("Server is live..."));
+
+app.use("/api/users", userRouter);
+app.use("/api/resumes", resumeRouter);
+app.use("/api/ai", aiRouter);
